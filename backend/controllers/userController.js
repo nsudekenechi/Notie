4253adc114c646e102dac7837cc2329537c61430
nsudekenechi <nsudekenechi2@gmail.com>
2023-06-30 00:01:19 +0100

    Created login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,8 +14,23 @@ const signUp = async (req, res) => {
         res.status(404).json(err.message)
     }
 }
+
+
 const login = async (req, res) => {
-    res.status(200).json({ message: "Created..." })
+    try {
+        const { fullname, password } = req.body;
+        const user = await model.find({ $or: [{ fullname }, { email: fullname }] });
+        if (!user) res.status(404).json({ message: "User not found" });
+
+
+        if (await bycrypt.compare(password, user[0].password)) {
+            res.status(200).json(user[0])
+        } else {
+            res.status(404).json({ message: "Invalid Password" })
+        }
+    } catch (err) {
+        res.status(404).json(err.message)
+    }
 }
 
 module.exports = { signUp, login }
