b396d9e80ca1cca650651bcae03c106b32ff8c98
nsudekenechi <nsudekenechi2@gmail.com>
2023-06-29 22:28:58 +0100

    Created Signup Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,18 @@
+const model = require("../models/userModel")
+const bycrypt = require("bcryptjs")
 const signUp = async (req, res) => {
-    res.status(200).json({ message: "Created..." })
+    try {
+        const { fullname, password, email } = req.body;
+        const hashed = await bycrypt.hash(password, 10);
+        const user = await model.create({
+            fullname,
+            password: hashed,
+            email
+        })
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(404).json(err.message)
+    }
 }
 const login = async (req, res) => {
     res.status(200).json({ message: "Created..." })
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     fullname: String,
-    email: String,
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
     password: String,
-    job: String
 
 })
 
