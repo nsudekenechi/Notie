b36f2f423932ac800be638eff22d966a8eebee68
nsudekenechi <nsudekenechi2@gmail.com>
2023-10-21 04:24:37 +0100

    Modified

--- frontend/src/pages/CreateNote.jsx
+++ frontend/src/pages/CreateNote.jsx
@@ -7,6 +7,12 @@ export const CreateNote = () => {
     selectedColor: ""
   });
 
+  const [inputs, setInputs] = useState({
+    title: "",
+    subtitle: "",
+    color: color.selectedColor
+  })
+
   const handleSelectedColor = (selected) => {
     setColor({ ...color, selectedColor: selected })
   }
@@ -18,14 +24,7 @@ export const CreateNote = () => {
           {color.colors.map((item, index) => (
             <div key={index} onClick={() => handleSelectedColor(item)} className={`w-5 h-5 rounded-full ${item == color.selectedColor ? "outline outline-green-300" : ""}`} style={{ backgroundColor: item }}></div>
           ))}
-      Your company has datacenters in Los Angeles and New York. The company has a Microsoft Azure subscription.
-You are configuring the two datacenters as geo-clustered sites for site resiliency.
-You need to recommend an Azure storage redundancy option.
-You have the following data storage requirements:
-✑ Data must be stored on multiple nodes.
-✑ Data must be stored on nodes in separate geographic locations.
-✑ Data can be read from the secondary location as well as from the primary location
-Which of the following Azure stored redundancy options should you recommend?  </div>
+        </div>
 
         <div className="grid grid-cols-1 pr-10">
           <div className="col-span-1">
