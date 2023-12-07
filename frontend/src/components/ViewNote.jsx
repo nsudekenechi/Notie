6c3d724235def1661279372e3dd7eb1ee8c33d41
Kenechi <nsudekenechi2@gmail.com>
2023-12-07 06:20:58 +0100

    Finished Responsiveness ViewNote

--- frontend/src/components/ViewNote.jsx
+++ frontend/src/components/ViewNote.jsx
@@ -18,26 +18,26 @@ export const ViewNote = () => {
   const scroll = useAnimateElementOnScroll();
 
   return (
-    <motion.div className={`col-span-6 md:px-10 py-10 min-h-screen flex flex-col gap-y-5 ${color.bg[color.circle.findIndex(item => item == note?.color)]}`}>
-      <Link to={"/dashboard/note"} className={`w-8 h-8 rounded-full   flex justify-center items-center  `}>
+    <motion.div className={`col-span-6 px-5 md:px-10 py-10 min-h-screen flex flex-col gap-y-5 ${color.bg[color.circle.findIndex(item => item == note?.color)]}`}>
+      <Link to={"/dashboard/note"} className={`w-8 h-8 rounded-full   flex justify-center items-center  text-xl md:text-lg`}>
         <HiArrowLeft className={`${color.text[color.circle.findIndex(item => item == note?.color)]}`} />
       </Link>
-      <div className="flex items-center gap-x-2">
+      <div className="flex items-center gap-x-2 lg:text-sm">
         <HiClock />
-        <p className="italic text-sm">{note?.date}</p>
+        <p className="italic ">{note?.date}</p>
       </div>
       <h1 className="text-3xl font-extrabold">{note?.title}</h1>
-      <p className="">{note?.subtitle}</p>
+      <p className="text-lg md:text-sm">{note?.subtitle}</p>
 
       <Link to={`/dashboard/note/editnote/${note?._id}`}>
         <motion.div
-          className={`w-[40px] h-[40px] fixed  duration-1000 shadow-xl  z-10 rounded-full  flex justify-center items-center text-white text-lg cursor-pointer top-[85%] right-5 ${color.audio[color.circle.findIndex(item => item == note?.color)]}`}
+          className={`w-[60px] h-[60px] md:w-[40px] lg:h-[40px] fixed  duration-1000 shadow-xl  z-10 rounded-full  flex justify-center items-center text-white text-lg cursor-pointer top-[85%] right-5 ${color.audio[color.circle.findIndex(item => item == note?.color)]}`}
           initial={{ scale: 1 }}
           animate={{ scale: scroll ? 0.7 : 1 }}
           transition={{ delay: 0.1 }}
 
         >
-          <HiPencil />
+          <HiPencil className="text-2xl md:text-lg"/>
         </motion.div>
       </Link>
     </motion.div>
